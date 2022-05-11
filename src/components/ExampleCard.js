f720cd4060cc3ccd0f1ea531595ebba64800a368
Rashmi Nagpal <rashminagpal.nagpal@gmail.com>
2022-05-11 23:20:39 +0530

    Added all the components

---
+++ src/components/ExampleCard.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const ExampleCard = styled.h1`
+  background: #025c0f;
+  color: #faf0f0;
+  line-height: 100px;
+  text-align: center;
+  font-size: 40px;
+  margin: 20px 20px;
+  padding: 2%;
+  position: relative;
+  box-shadow: 5px 5px 5px 0 #c6e0c1;
+`;
+
+export default ExampleCard;
